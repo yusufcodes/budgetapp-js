4a27f3f6030fc511072a432466ee2b6cbc1b87d5
yusufcodes <yusufcodes@outlook.com>
2019-10-23 10:10:21 +0100

    Added method to display the budget to the UI

--- app.js
+++ app.js
@@ -123,7 +123,11 @@ var UIController = ( function()
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
 
     };
 
@@ -179,6 +183,25 @@ var UIController = ( function()
             
         },
 
+        displayBudget: function(obj)
+        {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            
+
+            if (obj.percentage > 0)
+            {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            }
+            else
+            {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+
+
+        },
+
         // getDOMStrings: Returns the DOMStrings to be used elsewhere in the application
         getDOMstrings: function()
         {
@@ -216,7 +239,7 @@ var controller = ( function(budgetCtrl, UICtrl)
         var budget = budgetCtrl.getBudget();
 
         // Display the budget in the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     }
 
     // ctrlAddItem: The main controller method facilitating the addition of an item to the application
@@ -247,6 +270,12 @@ var controller = ( function(budgetCtrl, UICtrl)
         init: function()
         {
             console.log('Application has started');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     }
