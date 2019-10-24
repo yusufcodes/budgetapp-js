d72b62a232611991be00019230ad575d94f4673c
yusufcodes <yusufcodes@outlook.com>
2019-10-24 10:06:20 +0100

    Code written to display the percentages that were calculated to the UI

--- app.js
+++ app.js
@@ -186,7 +186,8 @@ var UIController = ( function()
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 
     };
 
@@ -267,6 +268,32 @@ var UIController = ( function()
 
         },
 
+        displayPercentages: function(percentages)
+        {
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback)
+            {
+                for (var i = 0; i < list.length; i++)
+                {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current,index)
+            {
+                if (percentages[index] > 0)
+                {
+                    current.textContent = percentages[index] + '%';
+                }
+                else
+                {
+                    current.textContent = '---';
+                }
+                
+            });
+        },
+
         // getDOMStrings: Returns the DOMStrings to be used elsewhere in the application
         getDOMstrings: function()
         {
@@ -318,7 +345,7 @@ var controller = ( function(budgetCtrl, UICtrl)
         var percentages = budgetCtrl.getPercentages();
 
         // Update the UI
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
     // ctrlAddItem: The main controller method facilitating the addition of an item to the application
