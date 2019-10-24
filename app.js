23d3bcd17a4d5a8c5b72531870d33e80331adea7
yusufcodes <yusufcodes@outlook.com>
2019-10-24 10:48:40 +0100

    Improved UX to change input fields focus colour depending on the expense type

--- app.js
+++ app.js
@@ -219,6 +219,14 @@ var UIController = ( function()
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function(list, callback)
+    {
+        for (var i = 0; i < list.length; i++)
+        {
+            callback(list[i], i);
+        }
+    };
+
     return {
         // getInput: Retrieves the input from the user for their expense
         getInput: function()
@@ -304,14 +312,6 @@ var UIController = ( function()
         {
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback)
-            {
-                for (var i = 0; i < list.length; i++)
-                {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current,index)
             {
                 if (percentages[index] > 0)
@@ -337,6 +337,23 @@ var UIController = ( function()
 
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
+        changedType: function() 
+        {
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
 
         },
 
@@ -369,6 +386,10 @@ var controller = ( function(budgetCtrl, UICtrl)
 
         // Event Delegation: all incomes and expenses will be assigned the event ctrlDeleteItem
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+
+
     };
 
     var updateBudget = function() 
