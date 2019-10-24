13db5652d80a553b02499c9c2a12bade25317b7a
yusufcodes <yusufcodes@outlook.com>
2019-10-24 10:40:53 +0100

    Displayed the current date

--- app.js
+++ app.js
@@ -187,7 +187,8 @@ var UIController = ( function()
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     };
 
@@ -325,6 +326,20 @@ var UIController = ( function()
             });
         },
 
+        displayMonth: function()
+        {
+            var now, month, year;
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
 
         // getDOMStrings: Returns the DOMStrings to be used elsewhere in the application
         getDOMstrings: function()
@@ -435,6 +450,7 @@ var controller = ( function(budgetCtrl, UICtrl)
         init: function()
         {
             console.log('Application has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
